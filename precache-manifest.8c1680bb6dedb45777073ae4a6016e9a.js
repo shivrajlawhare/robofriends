55a33dd9c995bc3023f0f27dda0587e77d402a3f
shivrajlawhare <113511366+shivrajlawhare@users.noreply.github.com>
2023-08-05 15:36:15 +0530

    Updates

---
+++ precache-manifest.8c1680bb6dedb45777073ae4a6016e9a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "de742c4e32c3eef64a6f",
+    "url": "/robofriends/static/css/main.88aff0f4.chunk.css"
+  },
+  {
+    "revision": "de742c4e32c3eef64a6f",
+    "url": "/robofriends/static/js/main.de742c4e.chunk.js"
+  },
+  {
+    "revision": "2981538dd718ef902032",
+    "url": "/robofriends/static/js/1.2981538d.chunk.js"
+  },
+  {
+    "revision": "0c52840a3e0443d6cd23",
+    "url": "/robofriends/static/css/2.bba05e8a.chunk.css"
+  },
+  {
+    "revision": "0c52840a3e0443d6cd23",
+    "url": "/robofriends/static/js/2.0c52840a.chunk.js"
+  },
+  {
+    "revision": "a7a4787743094c01098b",
+    "url": "/robofriends/static/js/runtime~main.a7a47877.js"
+  },
+  {
+    "revision": "9fdeb2491055e8810c615d354fd7cd0f",
+    "url": "/robofriends/index.html"
+  }
+];
